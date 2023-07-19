aa30e16f3a6e292b51a8b2a8cd3e04a9c37faffb
vitalMelnikofff <melnickov.vitalick2013@yandex.ru>
2023-07-20 00:32:26 +0300

    Task 2.3.2

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -1,9 +1,10 @@
 package web.config;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +19,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
 
-    public WebConfig(ApplicationContext applicationContext) {
+    @Autowired
+    public WebConfig(ApplicationContext applicationContext, Environment ignoredEnv) {
         this.applicationContext = applicationContext;
     }
 
